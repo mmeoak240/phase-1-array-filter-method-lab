b84e2fff1bbd03825bc6684384229bdf64f05d27
Michael Meoak <mmeoak240@gmail.com>
2022-03-05 19:46:52 -0700

    commit tree a7b43df16cc9c0bee91ebad4571a01eb8e3b069a

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solution here
+
+// const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby'];
+
+// const findMatching = (driver, string) => {
+//  return driver.filter(name => name.includes(string))
+// }
+
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(array, string) {
+    return array.filter(function(el) {
+        return el.toLowerCase().indexOf(string.toLowerCase())!== -1
+    })
+}
+
+// function fuzzyMatch(array, string) {
+//   return array.filter(name => name.includes(string));
+// }
+
+function fuzzyMatch(array, string){
+  return array.filter(function(el) {
+      return el.startsWith(string)
+  })
+}
+
+function matchName(drivers, name){
+  return drivers.filter(function(hometown){
+    return hometown.name === name
+  })
+}
